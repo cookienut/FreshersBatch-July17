903d8b6df12cae80032d836f8bb6578b61f66de8
sagarbhat94 <sagarbhat94@gmail.com>
2017-08-08 01:41:40 +0530

    Add files via upload

---
+++ Queue.java
@@ -0,0 +1,42 @@
+public class Queue{
+	
+	private int front;
+	private int rear;
+	private int arr[]=new int[8]; 
+	public static int MAXLENGTH=8;
+	
+	public Queue(){
+		front=0;
+		rear=0;
+	}
+	
+	public void enqueue(int newElement){
+		
+		arr[rear]=newElement;
+		rear++
+	}
+	
+	
+	
+	public void displayQueue(){
+		
+		int i;
+		for(i=0;i<=this.top;i++){
+			System.out.println("\n--\n"+arr[i]+"\n--");
+		}
+	}
+	
+	public static void main(String args[]){
+		
+		Queue q= new Queue();
+		int element;
+		q.enqueue(10);
+		q.enqueue(13);
+		q.enqueue(14);
+		q.displayQueue();
+
+		element=q.dequeue();
+		System.out.println("\nDequed Element:"+element);
+		
+	}
+}	
